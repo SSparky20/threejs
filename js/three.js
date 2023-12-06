ba680847e31093f914d7cded1467f3de3c3f2b17
zachr <zach.ryan1@icloud.com>
2023-12-05 22:52:29 -0700

    mask time

--- js/three.js
+++ js/three.js
@@ -11,11 +11,11 @@ let scene, camera, renderer;
         scene = new THREE.Scene();
         scene.background = new THREE.Color(0xdddddd);
 
-        camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+        camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,8000);
         camera.rotation.y = 45/180*Math.PI;
-        camera.position.x = 800;
-        camera.position.y = 100;
-        camera.position.z = 1000;
+        camera.position.x = 1000;
+        camera.position.y = 1000;
+        camera.position.z = 5000;
 
         renderer = new THREE.WebGLRenderer({antialias:true});
         renderer.setSize(window.innerWidth,window.innerHeight);
